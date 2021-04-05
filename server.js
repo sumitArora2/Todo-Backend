8979e5ffaa3eeb5e3117a398a1aa07fad4e38798
sumit <arora.sumit857@gmail.com>
2021-04-05 22:02:07 +0530

    changes

--- server.js
+++ server.js
@@ -32,12 +32,12 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/upload", uploadRoutes);
 
-app.get("/api/config/paypal", (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-);
+// app.get("/api/config/paypal", (req, res) =>
+//   res.send(process.env.PAYPAL_CLIENT_ID)
+// );
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "/frontend/build")));
