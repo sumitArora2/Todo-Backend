db32b892522bc6d10f14fa47dcae339981bbeba5
sumit <arora.sumit857@gmail.com>
2021-04-11 18:23:38 +0530

    added phone number in models

--- models/userModel.js
+++ models/userModel.js
@@ -16,6 +16,10 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    phoneNumber:{
+      type:Number,
+      required:true
+    },
     isAdmin: {
       type: Boolean,
       required: true,
