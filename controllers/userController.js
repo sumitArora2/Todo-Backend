f50dd0d7c06328005788163ee3fb774c1f54eb60
sumit <arora.sumit857@gmail.com>
2021-04-19 08:45:43 +0530

    otp login changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,19 +28,20 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, firebaseUid, phoneNumber } = req.body;
 
-  const userExists = await User.findOne({ email });
+  // const userExists = await User.findOne({ email });
 
-  if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
-  }
+  // if (userExists) {
+  //   res.status(400);
+  //   throw new Error("User already exists");
+  // }
 
   const user = await User.create({
     name,
     email,
-    password,
+    firebaseUid,
+    phoneNumber
   });
 
   if (user) {
@@ -49,7 +50,9 @@ const registerUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: generateToken(user._id),
+      phoneNumber:user.phoneNumber,
+      firebaseUid:user.firebaseUid
+      // token: generateToken(user._id),
     });
   } else {
     res.status(400);
